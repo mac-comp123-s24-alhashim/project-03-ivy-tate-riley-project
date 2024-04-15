6e6b4a9e832c614def57596844ba365343c2c8e9
TateMack <tmack1@macalester.edu>
2024-04-15 14:01:24 -0500

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -1,3 +1,7 @@
+import tkinter as tk
+
+
+
 def check_connection(button_click1, button_click2, button_click3, button_click4):
     if button_click1 == button_click2:
         if button_click1 == button_click3:
@@ -9,7 +13,7 @@ def check_connection(button_click1, button_click2, button_click3, button_click4)
                     self.button7.configure(bg="yellow")
                     self.button9.configure(bg="yellow")
                     self.button12.configure(bg="yellow")
-                    ##add list clearing feature 
+                    ##add list clearing feature
 
                 elif button_click1 == 2:
                     txt = self.correct_label[""]
@@ -35,3 +39,15 @@ def check_connection(button_click1, button_click2, button_click3, button_click4)
                     self.button14.configure(bg="purple")
                     self.button16.configure(bg="purple")
                     ##add list clearing feature
+
+            else:
+                txt = self.correct_label[""]
+                self.correct_label[""] = str("Incorrect")
+
+        else:
+            txt = self.correct_label[""]
+            self.correct_label[""] = str("Incorrect")
+
+    else:
+        txt = self.correct_label[""]
+        self.correct_label[""] = str("Incorrect")
