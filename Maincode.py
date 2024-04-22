397274edc1a1b495359babfca9794c7094a83f0f
igarrity <igarrity@macalester.edu>
2024-04-22 14:16:31 -0500

    added individual words to each button; working on function to add four button click texts to a list to then check if the four words are associated with the same category

--- Maincode.py
+++ Maincode.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from NewGUI import *
 
 word_list = {"Number Anagrams" : ["eon", "there", "net", "tow"],
              "Orderly" : ["clean", "neat", "tidy", "trim"],
@@ -21,61 +22,68 @@ def random_words(lst):
 
     return random_words
 
-print(random_words(word_list))
+list_of_words = random_words(word_list)
+print(list_of_words)
 
 
 
 
+def check_connection(self, button_click1, button_click2, button_click3, button_click4):
+    button_clicks = []
+    for x in range(4):
+        text = self.button.text()
+        button_clicks = button_clicks + text
+    return button_clicks
+    # if button_click1 == button_click2:
+    #     if button_click1 == button_click3:
+    #         if button_click1 == button_click4:
+    #             if button_click1 == 1:
+    #                 txt=self.correct_label[""]
+    #                 self.correct_label[""] = str("Correct! The Connection Category is Number Anagrams")
+    #                 self.button1.configure(bg="yellow")
+    #                 self.button7.configure(bg="yellow")
+    #                 self.button9.configure(bg="yellow")
+    #                 self.button12.configure(bg="yellow")
+    #                 ##add list clearing feature
+    #
+    #             elif button_click1 == 2:
+    #                 txt = self.correct_label[""]
+    #                 self.correct_label[""] = str("Correct! The Connection Category is Orderly")
+    #                 self.button2.configure(bg="green")
+    #                 self.button4.configure(bg="green")
+    #                 self.button11.configure(bg="green")
+    #                 self.button13.configure(bg="green")
+    #                 ##add list clearing feature
+    #             elif button_click1 == 3:
+    #                 txt = self.correct_label[""]
+    #                 self.correct_label[""] = str("Correct! The Connection Category is Drum___")
+    #                 self.button3.configure(bg="blue")
+    #                 self.button5.configure(bg="blue")
+    #                 self.button10.configure(bg="blue")
+    #                 self.button15.configure(bg="blue")
+    #                 ##add list clearing feature
+    #             elif button_click1 == 4:
+    #                 txt = self.correct_label[""]
+    #                 self.correct_label[""] = str("Correct! The Connection Category is Python Object Types")
+    #                 self.button6.configure(bg="purple")
+    #                 self.button8.configure(bg="purple")
+    #                 self.button14.configure(bg="purple")
+    #                 self.button16.configure(bg="purple")
+    #                 ##add list clearing feature
+    #
+    #         else:
+    #             txt = self.correct_label[""]
+    #             self.correct_label[""] = str("Incorrect")
+    #
+    #     else:
+    #         txt = self.correct_label[""]
+    #         self.correct_label[""] = str("Incorrect")
+    #
+    # else:
+    #     txt = self.correct_label[""]
+    #     self.correct_label[""] = str("Incorrect")
 
 
 
-
-# def check_connection(button_click1, button_click2, button_click3, button_click4):
-#     if button_click1 == button_click2:
-#         if button_click1 == button_click3:
-#             if button_click1 == button_click4:
-#                 if button_click1 == 1:
-#                     txt=self.correct_label[""]
-#                     self.correct_label[""] = str("Correct! The Connection Category is Number Anagrams")
-#                     self.button1.configure(bg="yellow")
-#                     self.button7.configure(bg="yellow")
-#                     self.button9.configure(bg="yellow")
-#                     self.button12.configure(bg="yellow")
-#                     ##add list clearing feature
-#
-#                 elif button_click1 == 2:
-#                     txt = self.correct_label[""]
-#                     self.correct_label[""] = str("Correct! The Connection Category is Orderly")
-#                     self.button2.configure(bg="green")
-#                     self.button4.configure(bg="green")
-#                     self.button11.configure(bg="green")
-#                     self.button13.configure(bg="green")
-#                     ##add list clearing feature
-#                 elif button_click1 == 3:
-#                     txt = self.correct_label[""]
-#                     self.correct_label[""] = str("Correct! The Connection Category is Drum___")
-#                     self.button3.configure(bg="blue")
-#                     self.button5.configure(bg="blue")
-#                     self.button10.configure(bg="blue")
-#                     self.button15.configure(bg="blue")
-#                     ##add list clearing feature
-#                 elif button_click1 == 4:
-#                     txt = self.correct_label[""]
-#                     self.correct_label[""] = str("Correct! The Connection Category is Python Object Types")
-#                     self.button6.configure(bg="purple")
-#                     self.button8.configure(bg="purple")
-#                     self.button14.configure(bg="purple")
-#                     self.button16.configure(bg="purple")
-#                     ##add list clearing feature
-#
-#             else:
-#                 txt = self.correct_label[""]
-#                 self.correct_label[""] = str("Incorrect")
-#
-#         else:
-#             txt = self.correct_label[""]
-#             self.correct_label[""] = str("Incorrect")
-#
-#     else:
-#         txt = self.correct_label[""]
-#         self.correct_label[""] = str("Incorrect")
+myGui = BasicGui(list_of_words)
+myGui.run()
--- NewGUI.py
+++ NewGUI.py
@@ -1,16 +1,28 @@
 import tkinter as tk
 from Maincode import *
+import cv2
+
 class BasicGui:
-    def __init__(self):
+    def __init__(self, list_of_words):
         self.rootWin = tk.Tk()
         self.rootWin.title("Connections")
         self.rootWin.geometry("750x750")
+        word_index = 0
         for row in range(4):
             for column in range(4):
-                self.button = tk.Button(self.rootWin, text=random_words(word_list), height=5, width=10)
+                self.button = tk.Button(self.rootWin, text=list_of_words[word_index], height=5, width=10)
                 self.button.grid(row=row, column=column)
+                word_index = word_index + 1
+
+    def check_connection(self, button_click1, button_click2, button_click3, button_click4):
+        button_clicks = []
+        for x in range(4):
+            text = self.button.text()
+            button_clicks = button_clicks + text
+        return button_clicks
+
+
     def run(self):
         self.rootWin.mainloop()
+        print(check_connection())
 
-myGui = BasicGui()
-myGui.run()
